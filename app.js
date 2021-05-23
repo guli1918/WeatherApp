76ed7416ab0b0a74fe5353d54348f2b0e92dbe46
guli1918 <mustafadennisozdemir@gmail.com>
2021-05-23 19:25:16 +0300

    wheather app v0.1

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.urlencoded({extended: true}));
+
+const https = require('https');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.cityName)
+    const query = req.body.cityName;
+    const apiKey = '5ad94d5ae9cd45f419a37ab6e0ce3591';
+    const unit = 'metric'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&appid=${apiKey}`;
+    
+    https.get(url, (respond) => {
+        respond.on('data', (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+
+            res.write("<p>The weather is currently " + description + "</p>");
+            res.write(`<h1>The temperature in ${req.body.cityName} is ${temp} degrees!</h1>`);
+            res.write(`<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`)
+        })
+        process.on('uncaughtException', (err) => {
+            res.write(`<h1 style="color:red"> You entered ${req.body.cityName} but it is not a city!</h1>`);
+            res.write(`<a href="/">Go Back</a>`);
+        })
+    });
+    
+
+})
+
+app.listen(port, () =>{
+    console.log(`Server is running on ${port}`);
+})
+
